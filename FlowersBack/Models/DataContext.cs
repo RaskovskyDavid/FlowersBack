d6b48f6b515ce1fb00d787332efb2a2d6590aba3
David Ariel Raskovsky <davidtucu@hotmail.com>
2017-11-26 10:14:03 -0300

    Finalizado

---
+++ FlowersBack/Models/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace FlowersBack.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+        public System.Data.Entity.DbSet<FlowersBack.Models.Flower> Flowers { get; set; }
+    }
+}
