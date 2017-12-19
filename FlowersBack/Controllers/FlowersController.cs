6c88a2e968c7bdeb55773954c13e92bbe0897423
David Ariel Raskovsky <davidtucu@hotmail.com>
2017-12-19 13:27:39 -0300

    ABM Imagenes funcionando

--- FlowersBack/Controllers/FlowersController.cs
+++ FlowersBack/Controllers/FlowersController.cs
@@ -6,18 +6,23 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using FlowersBack.Clases;
 using FlowersBack.Models;
 
 namespace FlowersBack.Controllers
 {
     public class FlowersController : Controller
     {
-        private DataContext db = new DataContext();
+        private DataContext db ;
 
+        public FlowersController()
+        {
+            db = new DataContext();
+        }
         // GET: Flowers
         public ActionResult Index()
         {
-            return View(db.Flowers.ToList());
+            return View(db.Flowers.OrderBy(f => f.Description).ToList());
         }
 
         // GET: Flowers/Details/5
@@ -27,7 +32,7 @@ namespace FlowersBack.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Flower flower = db.Flowers.Find(id);
+            var flower = db.Flowers.Find(id);
             if (flower == null)
             {
                 return HttpNotFound();
@@ -46,16 +51,42 @@ namespace FlowersBack.Controllers
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FlowerId,Description,Price")] Flower flower)
+        public ActionResult Create(
+            //[Bind(Include = "FlowerId,Description,Price,LastPurchase,Image,IsActive,Observation")]
+        FlowerView view)
         {
             if (ModelState.IsValid)
             {
+                var pic = string.Empty;
+                var folder = "~/Content/Images";
+
+                if (view.ImageFile != null)
+                {
+                    pic = FilesHelper.UploadPhoto(view.ImageFile, folder);
+                    pic = string.Format("{0}/{1}", folder, pic);
+                }
+                var flower = ToFlower(view);
+                flower.Image = pic;
                 db.Flowers.Add(flower);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(flower);
+            return View(view);
+        }
+
+        private Flower ToFlower(FlowerView view)
+        {
+            return new Flower
+            {
+                Description = view.Description,
+                FlowerId = view.FlowerId,
+                Image = view.Image,
+                IsActive = view.IsActive,
+                LastPurchase = view.LastPurchase,
+                Observation = view.Observation,
+                Price = view.Price,
+            };
         }
 
         // GET: Flowers/Edit/5
@@ -70,7 +101,21 @@ namespace FlowersBack.Controllers
             {
                 return HttpNotFound();
             }
-            return View(flower);
+            return View(ToView(flower));
+        }
+
+        private FlowerView ToView(Flower flower)
+        {
+            return new FlowerView
+            {
+                Description = flower.Description,
+                FlowerId = flower.FlowerId,
+                Image = flower.Image,
+                IsActive = flower.IsActive,
+                LastPurchase = flower.LastPurchase,
+                Observation = flower.Observation,
+                Price = flower.Price,
+            };
         }
 
         // POST: Flowers/Edit/5
@@ -78,15 +123,27 @@ namespace FlowersBack.Controllers
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "FlowerId,Description,Price")] Flower flower)
+        public ActionResult Edit(
+            //[Bind(Include = "FlowerId,Description,Price,LastPurchase,Image,IsActive,Observation")]
+        FlowerView view)
         {
             if (ModelState.IsValid)
             {
+                var pic = view.Image;
+                var folder = "~/Content/Images";
+
+                if (view.ImageFile != null)
+                {
+                    pic = FilesHelper.UploadPhoto(view.ImageFile, folder);
+                    pic = string.Format("{0}/{1}", folder, pic);
+                }
+                var flower = ToFlower(view);
+                flower.Image = pic;
                 db.Entry(flower).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(flower);
+            return View(view);
         }
 
         // GET: Flowers/Delete/5
--- FlowersBack/Models/FlowerView.cs
+++ FlowersBack/Models/FlowerView.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Web;
 
 namespace FlowersBack.Models
 {
-    public class Flower
+    public class FlowerView 
     {
-        [Key]
+       
         public int FlowerId { get; set; }
 
         [Required(ErrorMessage = "You must enter a {0}")]
         [StringLength(50, ErrorMessage = "The field {0} can contain maximun {1} and minimum {2} characters", MinimumLength = 1)]
-        [Index("Flower_Description_Index", IsUnique = true)]
         public string Description { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -22,15 +21,21 @@ namespace FlowersBack.Models
         public decimal Price { get; set; }
 
         [DataType(DataType.Date)]
-        [Display(Name ="Last purchase")]
+        [Display(Name = "Last purchase")]
         public DateTime? LastPurchase { get; set; }
 
-        public String Image { get; set; }
+       
 
-        [Display(Name ="Is active")]
+        [Display(Name = "Is active")]
         public bool IsActive { get; set; }
 
         [DataType(DataType.MultilineText)]
         public String Observation { get; set; }
+
+
+        public String Image { get; set; }
+
+        [Display(Name = "Image")]
+        public HttpPostedFileBase ImageFile { get; set; }
     }
 }
